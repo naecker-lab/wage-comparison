5ab21557fefcb5cc0145e21808c3463541732a8e
Sarah Xu <sxu@wesleyan.edu>
2017-03-10 15:11:18 -0500

    set number of rounds to 5. created answer text field to get user input

--- app/triangle/models.py
+++ app/triangle/models.py
@@ -24,27 +24,30 @@ class Constants(BaseConstants):
    
 
     #Calls CSV from location within app folder
-    with open('triangle/triangle.csv') as f:
-        payoff_set = list(csv.DictReader(f))
+    #with open('triangle/triangle.csv') as f:
+        #payoff_set = list(csv.DictReader(f))
 
     #Defines number of rounds as number of rows in CSV
     #Additional rounds can be addded by adding rows to CSV
-    num_rounds = len(payoff_set)
+    #num_rounds = len(payoff_set)
+    num_rounds=5
+    guess_max = 150
 
 
 
 class Subsession(BaseSubsession):
 
     #Keeps track of which row of the CSV the  app is pulling from for the current question
-    def before_session_starts(self):
-        if self.round_number == 1:
-            self.session.vars['questions'] = Constants.payoff_set
+    #def before_session_starts(self):
+    #    if self.round_number == 1:
+     #       self.session.vars['questions'] = Constants.payoff_set
        
         #Gets current row and then selects columns to extracdt needed data
-        for p in self.get_players():
-            question_data = p.current_question()
-            p.question_id = question_data['id']
-            p.question = "For Set " + str(question_data['id']) + " , which point would you prefer?" 
+      #  for p in self.get_players():
+       #     question_data = p.current_question()
+        #    p.question_id = question_data['id']
+         #   p.question = "For Set " + str(question_data['id']) + " , which point would you prefer?" 
+         pass
 
 #Defines how groups opterate
 #Since we do not have groups, classis not used
@@ -54,10 +57,10 @@ class Group(BaseGroup):
 
 #Defines attributes for each player
 class Player(BasePlayer):
-    question_id = models.PositiveIntegerField()
-    question = models.CharField()
-    submitted_answer = models.CharField(widget=widgets.RadioSelect())
+    answer = models.PositiveIntegerField(max=Constants.guess_max)
+    #question = models.CharField()
+    #submitted_answer = models.CharField(widget=widgets.RadioSelect())
 
 
-    def current_question(self):
-        return self.session.vars['questions'][self.round_number - 1]
+    #def current_question(self):
+     #   return self.session.vars['questions'][self.round_number - 1]
--- app/triangle/views.py
+++ app/triangle/views.py
@@ -9,59 +9,58 @@ import numpy as np
 #This class sends information to the Questions.html page
 class Question(Page):
     form_model = models.Player
-    form_fields = ['submitted_answer']
+    form_fields = ['answer']
 
     #Creates text for answer options
-    def submitted_answer_choices(self):
-        qd = self.player.current_question()
-        #Numbers for Option A
-        a_p1 = int(100*float(qd['A_p1']))
-        a_p3 = int(100*float(qd['A_p3']))
-        a_p2 = int(100 - a_p1 - a_p3)
+     #def submitted_answer_choices(self):
+         #qd = self.player.current_question()
+    #     #Numbers for Option A
+    #     a_p1 = int(100*float(qd['A_p1']))
+    #     a_p3 = int(100*float(qd['A_p3']))
+    #     a_p2 = int(100 - a_p1 - a_p3)
         
-        #Numbers for Option B
-        b_p1 = int(100*float(qd['B_p1']))
-        b_p3 = int(100*float(qd['B_p3']))
-        b_p2 = int(100 - a_p1 - a_p3)
+    #     #Numbers for Option B
+    #     b_p1 = int(100*float(qd['B_p1']))
+    #     b_p3 = int(100*float(qd['B_p3']))
+    #     b_p2 = int(100 - a_p1 - a_p3)
 
-        #Returns dynamic text options for A and B
-        return [
-           str(a_p1)+"%"+" chance of "+qd['payoff1']+", "+str(a_p2)+"%"+" chance of "+qd['payoff2']+", or "+str(a_p3)+"%"+" chance of "+qd['payoff3'],
-           str(b_p1)+"%"+" chance of "+qd['payoff1']+", "+str(b_p2)+"%"+" chance of "+qd['payoff2']+", or "+str(b_p3)+"%"+" chance of "+qd['payoff3'],
-        ]
+    #     #Returns dynamic text options for A and B
+         #return [
+           # "How many zeros are in the table?"
+          #  }
 
     #Creates data series that is passed to imbeded highchart triangle
     #Data takes form [[a1,a2],[b1,b2]]
     def vars_for_template(self):
-        #Array for single set of points
-        pointA = []
-        pointB = []
-        #Array to hold 2 sets of points
-        points = []
-        qd = self.player.current_question()
+        # #Array for single set of points
+        # pointA = []
+        # pointB = []
+        # #Array to hold 2 sets of points
+        # points = []
+        # qd = self.player.current_question()
 
-        #Adding points for set A
-        pointA.append(float(qd['A_p1']))
-        pointA.append(float(qd['A_p3']))
+        # #Adding points for set A
+        # pointA.append(float(qd['A_p1']))
+        # pointA.append(float(qd['A_p3']))
 
-        #Adding points for set B
-        pointB.append(float(qd['B_p1']))
-        pointB.append(float(qd['B_p3']))
+        # #Adding points for set B
+        # pointB.append(float(qd['B_p1']))
+        # pointB.append(float(qd['B_p3']))
 
-        #Adding A and B to points
-        points.append(pointA)
-        points.append(pointB)
+        # #Adding A and B to points
+        # points.append(pointA)
+        # points.append(pointB)
 
-        #This line is needed for the data to be passed to highcharts
-        #Without it the data is in the wrong form and will crash program
-        points = safe_json(points)
+        # #This line is needed for the data to be passed to highcharts
+        # #Without it the data is in the wrong form and will crash program
+        # points = safe_json(points)
 
         m = np.random.randint(2, size=(10, 15))
         num_zero = (10*15) - np.count_nonzero(m)
 
         #Returns [[a1,a2],[b1,b2]] as a series
         return{
-            'series' : points,
+            #'series' : points,
             'matrix' : m,
             'num_zero' : num_zero,
         }
