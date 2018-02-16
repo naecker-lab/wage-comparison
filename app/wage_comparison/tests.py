c2550c7742683a8ec3da52a268d6363a997d1773
Sarah Xu <sxu@wesleyan.edu>
2018-02-16 00:47:32 -0500

    Trying bots - don't know

--- app/wage_comparison/tests.py
+++ app/wage_comparison/tests.py
@@ -3,6 +3,7 @@ from . import views
 from ._builtin import Bot
 from .models import Constants
 from .views import Question
+from otree.api import Submission
 
 
 class PlayerBot(Bot):
@@ -10,9 +11,12 @@ class PlayerBot(Bot):
     def play_round(self):
         # submitted_answer = self.player.current_question()['choice1']
         # answer = num_zero
-        yield(views.Introduction)
+        yield Submission(views.Introduction)
+        yield Submission(views.Question, {'contribution': 10}, check_html=False)
+
+        yield Submission(views.Results)
 
 
-        yield (views.Question, {'contribution': 50})
-        yield(views.Results)
+        # yield (views.Question, {'contribution': 50})
+        # yield(views.Results)
         
